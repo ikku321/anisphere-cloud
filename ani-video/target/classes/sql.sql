69a1920424a5c703fec4913b0c7d8f653ae8f824
Your Name <you@example.com>
2026-03-16 23:57:00 +0800

    完成了视频上传，视频封面图片上传功能

---
+++ ani-video/target/classes/sql.sql
@@ -0,0 +1,3 @@
+insert into ani_sphere.video(video_id, user_id, title, description, cover_url, video_url, duration, status, visible,price, audit_status)
+    VALUE ('test01', 'user1', '测试视频标题', '视频简介', '视频封面地址',
+           '视频链接地址', 1000, 0, 1, 0, 0);
